0fa760ad0b51090e401113741515590a8206c445
Che-cop <jguymon2002@yahoo.com>
2016-03-15 20:15:52 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpiritAndAnimal::Application.config.secret_key_base = '03a386541e08cf1d2842e30f183fa3dc87e08aefd55a68e9fa89da0fd9cb9a6246bb2ad8632d2e0fc30c9cf906148f698a13d6d94a59d2216b7ebfa8e1280c62'
